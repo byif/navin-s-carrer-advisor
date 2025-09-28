275f034fec3c3866e071373f8a7b10e7b7a611f9
byif <166938225+byif@users.noreply.github.com>
2025-09-28 18:59:04 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -219,9 +219,8 @@ async def websocket_endpoint(websocket: WebSocket):
                 analysis_results = {}
 
                 try:
-                
-                   results = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False, detector_backend='ssd')
-                if results:
+                    results = DeepFace.analyze(frame, actions=['emotion'], enforce_detection=False, detector_backend='ssd')
+                    if results:
                         dominant_emotion = results[0]['dominant_emotion']
                         analysis_results['emotion'] = dominant_emotion
                     else:
@@ -243,6 +242,4 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         print("Client disconnected.")
     except Exception as e:
-
         print(f"Unexpected error: {e}")
-
